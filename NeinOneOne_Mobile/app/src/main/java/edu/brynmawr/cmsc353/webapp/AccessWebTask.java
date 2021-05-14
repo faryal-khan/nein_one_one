4286be46aee9d24479d76ed7cc1ddf933707a75f
Nigina Daniyarova <daniyarova.nigina@gmail.com>
2021-05-14 03:04:14 -0400

    bugs are fixed

--- NeinOneOne_Mobile/app/src/main/java/edu/brynmawr/cmsc353/webapp/AccessWebTask.java
+++ NeinOneOne_Mobile/app/src/main/java/edu/brynmawr/cmsc353/webapp/AccessWebTask.java
@@ -25,8 +25,9 @@ import static androidx.core.content.ContextCompat.startActivity;
 
 public class AccessWebTask extends AsyncTask<URL, String, String> {
     HashMap<String, String[]> resources = new HashMap<>();
-    String[] attributes = new String[7];
-    String name, phone, website, description, lat, lon, location, zipcode;
+
+
+    String name, phone, website, description, lat, lon, location, zipcode = "";
     public HashMap<String, String[]> getResources(){
         return resources;
     }
@@ -34,8 +35,8 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
     public String getName(){
         return name;
     }
-    public String getPhone(){
-        return phone;
+    public String getLat(String name){
+        return resources.get(name)[5];
     }
     public String getDescription(){
         return description;
@@ -43,6 +44,7 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
     public String getWebsite(){
         return website;
     }
+
     public String doInBackground(URL... urls) {
 
         try {
@@ -58,59 +60,48 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
             JSONArray jo = new JSONArray(response);
 
             for (int i = 0; i < jo.length(); i++){
+                String[] attributes = new String[8];
+                for(int j = 0; j < 8; j++){
+                    attributes[j] = "";
+                }
                 name = jo.getJSONObject(i).get("name").toString();
-                //res += "name: " + name + "\n";
                 phone = jo.getJSONObject(i).get("phone").toString();
-                //res += "phone: " + phone + "\n";
-                if (jo.getJSONObject(i).get("website").toString() == null ||
-                        jo.getJSONObject(i).get("website").toString().equals(""))
-                    website = "";
-                 //   res += "";
-                else {
-                    website = jo.getJSONObject(i).get("website").toString();
-                    //res += "website: " + website + "\n";
-                }
-                if (jo.getJSONObject(i).get("description").toString() == null ||
-                        jo.getJSONObject(i).get("description").toString().equals(""))
-                    description = "";
-                    //res += "";
-                else {
-                    description = jo.getJSONObject(i).get("description").toString();
-                    //res += "description: " + website + "\n";
-                }
-                if (jo.getJSONObject(i).get("location").toString() == null ||
-                        jo.getJSONObject(i).get("location").toString().equals(""))
-                    location = "";
-                    //res += "";
-                else {
-                    location = jo.getJSONObject(i).get("location").toString();
-                    //res += "description: " + website + "\n";
-                }
-                if (jo.getJSONObject(i).get("zipcode").toString() == null ||
-                        jo.getJSONObject(i).get("zipcode").toString().equals(""))
-                    zipcode = "";
-                    //res += "";
-                else {
-                    location = jo.getJSONObject(i).get("zipcode").toString();
-                    //res += "description: " + website + "\n";
-                }
-                if (jo.getJSONObject(i).get("latitude").toString() == null ||
-                        jo.getJSONObject(i).get("latitude").toString().equals(""))
-                    lat = "";
-                    //res += "";
-                else {
-                    lat = jo.getJSONObject(i).get("latitude").toString();
-                    //res += "description: " + website + "\n";
-                }
-                if (jo.getJSONObject(i).get("longitude").toString() == null ||
-                        jo.getJSONObject(i).get("longitude").toString().equals(""))
-                    lon = "";
-                    //res += "";
-                else {
-                    lon = jo.getJSONObject(i).get("longitude").toString();
-                    //res += "description: " + website + "\n";
-                }
 
+                if (jo.getJSONObject(i).get("website") == null ||
+                        jo.getJSONObject(i).get("website").toString().equals("") ||
+                        jo.getJSONObject(i).get("website").toString().isEmpty())
+                    website = "";
+                else website = jo.getJSONObject(i).get("website").toString();
+
+                if (jo.getJSONObject(i).get("description") == null ||
+                        jo.getJSONObject(i).get("description").toString().equals("") ||
+                        jo.getJSONObject(i).get("description").toString().isEmpty())
+                    description = "";
+                else description = jo.getJSONObject(i).get("description").toString();
+
+                if (jo.getJSONObject(i).get("location") == null ||
+                        jo.getJSONObject(i).get("location").toString().equals("") ||
+                        jo.getJSONObject(i).get("location").toString().isEmpty())
+                    location = "";
+                else location = jo.getJSONObject(i).get("location").toString();
+
+                if (jo.getJSONObject(i).get("zipcode") == null ||
+                        jo.getJSONObject(i).get("zipcode").toString().equals("") ||
+                        jo.getJSONObject(i).get("zipcode").toString().isEmpty())
+                    zipcode = "";
+                else zipcode = jo.getJSONObject(i).get("zipcode").toString();
+
+                if (jo.getJSONObject(i).get("latitude") == null ||
+                        jo.getJSONObject(i).get("latitude").toString().equals("") ||
+                        jo.getJSONObject(i).get("latitude").toString().isEmpty())
+                    lat = "";
+                else lat = jo.getJSONObject(i).get("latitude").toString();
+
+                if (jo.getJSONObject(i).get("longitude") == null ||
+                        jo.getJSONObject(i).get("longitude").toString().equals("") ||
+                        jo.getJSONObject(i).get("longitude").toString().isEmpty())
+                    lon = "";
+                else lon = jo.getJSONObject(i).get("longitude").toString();
 
                 attributes[0] = phone;
                 attributes[1] = website;
@@ -119,11 +110,15 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
                 attributes[4] = zipcode;
                 attributes[5] = lat;
                 attributes[6] = lon;
+                attributes[7] = name;
 
-                //Log.v("age", description);
+                //Log.v("name1", name);
                 resources.put(name, attributes);
+                //attributes[0] = "";
             }
-            return res;
+            //return resources;
+            return resources.get("reston1")[0];
+            //return jo.getJSONObject(0).get("name").toString();
 
             /*
             res += "name: " + jo.get("name").toString() + "\n";
@@ -155,6 +150,7 @@ public class AccessWebTask extends AsyncTask<URL, String, String> {
 */
         }
         catch (Exception e) {
+
             String ex = "";
             if (phone == null && website == null && description == null)
                 return "There are no resources with this name";
--- NeinOneOne_Mobile/app/src/main/java/edu/brynmawr/cmsc353/webapp/SearchActivity.java
+++ NeinOneOne_Mobile/app/src/main/java/edu/brynmawr/cmsc353/webapp/SearchActivity.java
@@ -5,20 +5,30 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
 
 import java.net.URL;
 import java.util.HashMap;
 
 public class SearchActivity extends AppCompatActivity {
+    private static final int REQUEST_CALL = 1;
+    String phone, pname = "";
     EditText editTextSearch;
-    double myLongitude;
-    double myLatitude;
+    double myLongitude, myLatitude;
     LocationManager mLocationManager;
     Intent i = new Intent();
     HashMap<String, String[]> resources = new HashMap<>();
@@ -54,15 +64,14 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         editTextSearch = (EditText) findViewById(R.id.editSearch);
-
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1,
                 1, mLocationListener);
-    }
+
+}
 
     public void onSearchButtonClick(View v) {
-
         String result = "";
         TextView res = findViewById(R.id.result);
 
@@ -74,74 +83,86 @@ public class SearchActivity extends AppCompatActivity {
             else {
                 URL url = new URL("http://10.0.2.2:3000/api");
 
-                edu.brynmawr.cmsc353.project.AccessWebTask task = new edu.brynmawr.cmsc353.project.AccessWebTask();
+                AccessWebTask task = new AccessWebTask();
                 task.execute(url);
-                //String pwd;
-            /*
-            if ( editTextSearch.getText().toString() == null) {
-                pwd = "Please enter the name of the resource";
-            }
-            else {
-            */
+
+
+                String lat = task.get();
                 resources = task.getResources();
 
-                String pwd = task.get();
+                HashMap<String, String> phoneInd = new HashMap<>();
+                String indxs = "";
+
 
                 for(String name : resources.keySet()) {
                     if (name.contains(request)) {
                         resource = resources.get(name);
-                        if(distance(myLatitude, myLongitude, Double.parseDouble(resource[5]), Double.parseDouble(resource[6])) <= 10) {
+                        if(distance(myLatitude, myLongitude, Double.parseDouble(resource[5]),
+                                Double.parseDouble(resource[6])) <= 10) {
+                            pname = name;
                             result += "name: " + name + "\n";
-                            result += "phone: " + resource[0] + "\n";
-
-                            if (resource[1].equals("")) result += "";
+                            phone = resource[0];
+                            result += "phone: " + phone + "\n";
+                            indxs = String.valueOf(result.length()-phone.length() - 1) + "," + result.length();
+                            phoneInd.put(indxs, phone);
+                            if (resource[1].equals("") || resource[1].isEmpty()) result += "";
                             else result += "website: " + resource[1] + "\n";
-                            if (resource[2].equals("")) result += "";
+
+                            if (resource[2].equals("") || resource[2].isEmpty()) result += "";
                             else result += "description: " + resource[2] + "\n";
-                            result += "location: " + resource[3] + "\n";
-                            result += "zipcode: " + resource[4] + "\n";
-                            result += "latitude: " + resource[5] + "\n";
-                            result += "longitude: " + resource[6] + "\n";
+
+                            if (resource[3].equals("")|| resource[3].isEmpty()) result += "";
+                            else result += "location: " + resource[3] + "\n";
+
+                            if (resource[4].equals("") || resource[4].isEmpty()) result += "";
+                            else result += "zipcode: " + resource[4] + "\n";
+
+                            if (resource[5].equals("")|| resource[5].isEmpty()) result += "";
+                            else result += "latitude: " + resource[5] + "\n";
+
+                            if (resource[6].equals("")|| resource[6].isEmpty()) result += "";
+                            else result += "longitude: " + resource[6] + "\n";
                             result += "\n";
+
                         }
                     }
                 }
 
-                /*
-                if (resource[1].equals(""))
-                    result += "";
-                else result += "website: " + resource[1] + "\n";
-                if (resource[2].equals(""))
-                    result += "";
-                else result += "description: " + resource[2] + "\n";
-                if (resource[3].equals(""))
-                    result += "";
-                else result += "location: " + resource[3] + "\n";
-                if (resource[4].equals(""))
-                    result += "";
-                else result += "zipcode: " + resource[4] + "\n";
-                if (resource[5].equals(""))
-                    result += "";
-                else result += "latitude: " + resource[5] + "\n";
-                if (resource[6].equals(""))
-                    result += "";
-                else result += "longitude: " + resource[6] + "\n";
-*/
-
-                //pwd = "Phone: " + phone + "\n" + "Website: " + website
-                if (pwd == null) {
-                    pwd = "Sorry, there are no resources with this name";
+                if (result == null || result.equals("") || result.isEmpty()) {
+                    result = "Sorry, there are no resources with this name nearby";
+                    res.setText(result);
                 }
-
-                //  System.out.println("RESULT" + resources.keySet());
-
-                res.setText(result);
+                else {
+                    SpannableString ss = new SpannableString(result);
+                    for (String index : phoneInd.keySet()){
+                        ClickableSpan cs1 = new ClickableSpan() {
+                            @Override
+                            public void onClick(@NonNull View widget) {
+                                String dial = "tel:" + phoneInd.get(index);
+                                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                            }
+                        };
+                        int start = Integer.parseInt(index.substring(0,index.indexOf(',')));
+                        int finish = Integer.parseInt(index.substring(index.indexOf(',')+1));
+                        ss.setSpan(cs1, start, finish, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        //System.out.println(distance(myLatitude, myLongitude, Double.parseDouble(resource[5]),
+                        //        Double.parseDouble(resource[6])));
+                    }
+                    res.setText(ss);
+                    res.setMovementMethod(LinkMovementMethod.getInstance());
+                }
             }
         } catch (Exception e) {
             // uh oh
-            e.printStackTrace();
-            res.setText("The name is invalid or the resource was not found");
+            //e.printStackTrace();
+            res.setText(e.toString());
         }
+    }
+
+    public void onFinishButtonClick(View v) {
+
+        setResult(RESULT_OK, i);
+        finish();
     }
 
     private double distance(double lat1, double lng1, double lat2, double lng2) {
@@ -162,11 +183,5 @@ public class SearchActivity extends AppCompatActivity {
         double dist = earthRadius * c;
 
         return dist; // output distance, in MILES
-    }
-
-    public void onFinishButtonClick(View v) {
-
-        setResult(RESULT_OK, i);
-        finish();
     }
 }
